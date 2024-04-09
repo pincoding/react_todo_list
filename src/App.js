fbee3630a8c328aec3eb523b1bd76112b8e41aca
pincoding <dydtjswkdrns@naver.com>
2024-04-09 17:16:41 +0900

    todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Container, Heading, Input } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
-function App() {
+const App = () => {
+  const [todos, settodos] = useState(() => {
+    const getTodo = localStorage.getItem("todos");
+    return getTodo ? JSON.parse(getTodo) : [];
+    // JSON.parse json문자열의 구문을 분석하고, 그결과에서 자바스크립트로 반환한다
+  });
+
+  console.log(todos);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmitHandler = (data) => {
+    const { todo } = data;
+    settodos([
+      ...todos,
+      {
+        id: Date.now(),
+        text: todo,
+        finish: false,
+      },
+    ]);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+    // JSON.stringify 자바스크립트 값을 json형식으로 바꾼다
+  }, [todos]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Container
+      maxW={"450px"}
+      w={"100%"}
+      h={"100vh"}
+      margin={"0 auto"}
+      bgColor={"gray.100"}
+      p={"50px 20px"}
+    >
+      <Heading>WHAT TO DO</Heading>
 
+      <Box as="form" m={"30px 0"} onSubmit={handleSubmit(onSubmitHandler)}>
+        <Input
+          {...register("todo", {
+            required: "내용을 작성해주세요",
+          })}
+          placeholder="아이디를 입력해주세요"
+          borderColor={"gray.400"}
+          size={"md"}
+        />
+      </Box>
+    </Container>
+  );
+};
 export default App;
+// https://chakra-ui.com/ css ui
+// npm i @chakra-ui/react @emotion/react @emotion/styled framer-motion
+// npm i @fontsource/noto-sans-kr
